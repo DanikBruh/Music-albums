7648ee890afdf07195b8e9884b7b148e131e26cf
DanikBruh <danik.myrzaliev@mail.ru>
2021-02-03 10:34:21 +0600

    Added a table and one more album to db

--- frontend/work2.js
+++ frontend/work2.js
@@ -15,4 +15,37 @@ var app = new Vue({
             this.albums = res.data;
         });
     }
-  })
+})
+
+var table = document.getElementById('table');
+var array = table.children;
+var tableCells = [];
+for(i = 0; i < array.length; i++) {
+    for(j = 0; j < array[i].childElementCount; j++) {
+        for(k = 0; k < array[i].children[j].childElementCount; k++) {
+            tableCells.push(array[i].children[j].children[k])
+        }
+    }
+}
+
+var prev = 0;
+setInterval(function() {
+    var rand = Math.floor(Math.random() * Math.floor(tableCells.length))
+    while(rand == prev) {
+        rand = Math.floor(Math.random() * Math.floor(tableCells.length))
+    }
+    prev = rand
+    for(i = 0; i < tableCells.length; i++) {
+        ChangeToBlack(i);
+    }
+    ChangeToWhite(rand);
+}, 400);
+
+function ChangeToBlack (index) {
+    tableCells[index].style.background = "#333";
+    tableCells[index].style.color = "#fff";
+}
+function ChangeToWhite (index) {
+    tableCells[index].style.background = "#fff";
+    tableCells[index].style.color = "#333";
+}
